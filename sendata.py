f7804fa6aa91daa924a5207bd8a8511985a0c4a1
SonTT <thanhson12a12002@gmail.com>
2024-11-04 00:00:39 +0700

    d

--- sendata.py
+++ sendata.py
@@ -3,5 +3,5 @@ import time
 
 # Gửi dữ liệu 60 lần với các giá trị mẫu
 for i in range(2):
-    myTest.__sendData("Tag2", "222222", "Tag3", "3131", "TagDensity", "3131")
+    myTest.__sendData("Tag2", "1", "Tag3", "1", "TagDensity", "1")
     time.sleep(1)  # Thời gian nghỉ 1 giây giữa các lần gửi
